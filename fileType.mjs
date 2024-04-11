0352bd4e94e83e2a29203de6042eff5f241056ab
Camilo Montoya :) <camilo.montoya.contractor@macmillan.com>
2024-04-11 08:05:01 -0500

    Refactor file path handling in fileType.mjs

--- fileType.mjs
+++ fileType.mjs
@@ -1,6 +1,14 @@
 import { fileTypeFromFile } from 'file-type';
 
-const filePath = process.cwd() + '/' + process.argv[2];
+let filePath = '';
+if (process.argv[2]) {
+  filePath = `${process.cwd()}/${process.argv[2]}`;
+} else if (process.argv[1]) {
+  filePath = `${process.cwd()}/${process.argv[1]}`;
+} else {
+  throw new Error('No file path provided');
+}
+
 
 (async () => {
   const fileType = await fileTypeFromFile(filePath);
